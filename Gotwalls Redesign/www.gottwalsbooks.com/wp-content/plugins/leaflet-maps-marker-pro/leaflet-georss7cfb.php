c63b33920fcdaa254ef3778b0cf9cf65decc9b18
DiabeticDonut <devambrose23@gmail.com>
2022-02-17 22:34:21 -0500

    Gottwals website

---
+++ Gotwalls Redesign/www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-georss7cfb.php
@@ -0,0 +1,221 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:georss="http://www.georss.org/georss" xmlns:gml="http://www.opengis.net/gml" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
+<channel>
+<atom:link href="https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-georss.php?layer=all" rel="self" type="application/rss+xml" />
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?layer=0</link>
+<title>Gottwals Books - maps</title>
+<lastBuildDate>Mon, 22 May 2017 12:05:27 +1900</lastBuildDate>
+<generator>www.mapsmarker.com</generator>
+<description>GeoRSS-feed created with Maps Marker Pro (www.mapsmarker.com)</description>
+<item>
+<title>Warner Robins, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=1</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-1</guid>
+<pubDate>Thu, 17 Sep 2015 05:09:59 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Warner Robins, Georgia</em>Monday – Saturday:9:30 A.M.-6 P.M.(Close at 5pm on Wednesday)Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>32.593504 -83.665749</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Byron, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=2</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-2</guid>
+<pubDate>Fri, 18 Sep 2015 03:09:22 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Byron, Georgia</em>Monday – Saturday:9:30 A.M.-6 P.M.(Close at 5pm on Wednesday)Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>32.662959 -83.743106</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Tifton, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=3</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-3</guid>
+<pubDate>Fri, 18 Sep 2015 03:09:23 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Tifton, Georgia</em>Monday – Friday: 9:30 A.M.-6:00 P.M.Saturday: 9:30 A.M.-5:00 P.M.Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>31.451334 -83.509140</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Zanesville, Ohio</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=4</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-4</guid>
+<pubDate>Fri, 18 Sep 2015 03:09:02 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Zanesville, Ohio</em>Monday – Saturday:9:30 A.M.-6:00 P.M.Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>39.987701 -82.022555</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Macon, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=6</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-6</guid>
+<pubDate>Fri, 18 Sep 2015 03:09:02 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Macon, Georgia</em>Monday – Saturday: 9:30 A.M.-6 P.M.(Close at 5pm on Wednesday)Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>32.879846 -83.668701</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Perry, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=7</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-7</guid>
+<pubDate>Fri, 18 Sep 2015 03:09:02 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Perry, Georgia</em>Tuesday – Saturday: 10 A.M.-5 P.M.Sunday &amp; Monday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>32.458596 -83.733248</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>New Orleans, Louisiana</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=9</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-9</guid>
+<pubDate>Fri, 18 Sep 2015 03:09:02 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>New Orleans, Louisiana</em>Monday – Saturday: 10 A.M.-6:30 P.M.Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>29.964696 -90.062217</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Hartsville, South Carolina</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=10</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-10</guid>
+<pubDate>Fri, 18 Sep 2015 04:09:57 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Burry Bookstore &amp; Walls of Books</strong><em>Hartsville, South Carolina</em>Mon-Sat: 10:00am-9:00pm]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>34.373020 -80.076496</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Watkinsville, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=11</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-11</guid>
+<pubDate>Fri, 18 Sep 2015 04:09:58 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Watkinsville, Georgia</em>Monday – Friday: 9:30 A.M.-6:00 P.M.Saturday: 9:30 A.M.-5:00 P.M.Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>33.875273 -83.438035</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Ellijay, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=13</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-13</guid>
+<pubDate>Fri, 09 Oct 2015 08:10:50 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Ellijay, Georgia</em>Tuesday-Friday: 10:00am-6:00pmSaturday: 10:00am-5:00pmSunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>34.681737 -84.474594</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Washington, DC</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=14</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-14</guid>
+<pubDate>Thu, 19 Nov 2015 03:11:46 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Washington, DC</em>(202) 836-7374<strong></strong>Monday - Saturday: 10:00 A.M.-9:00 P.M.Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>38.931721 -77.023280</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Atchison, Kansas</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=15</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-15</guid>
+<pubDate>Fri, 19 Aug 2016 11:08:35 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Atchison, Kansas</em><strong></strong>]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>39.562117 -95.123454</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>West Chester, Ohio</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=16</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-16</guid>
+<pubDate>Fri, 19 Aug 2016 02:08:20 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>West Chester, OhioPlaza at Wetherington</em>]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>39.357761 -84.378004</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Commerce, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=18</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-18</guid>
+<pubDate>Fri, 03 Mar 2017 10:03:28 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Commerce, Georgia</em>Monday – Saturday: 10:00 A.M.-5:00 P.M.Sunday: Closed<em></em>]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>34.247981 -83.462732</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+<item>
+<title>Columbus, Georgia</title>
+<link>https://www.gottwalsbooks.com/wp-content/plugins/leaflet-maps-marker-pro/leaflet-fullscreen.php?marker=19</link>
+<guid isPermaLink="false">Gottwals_Books-layer-2-marker-19</guid>
+<pubDate>Mon, 22 May 2017 12:05:27 +1900</pubDate>
+<dc:creator>mandr</dc:creator>
+<description><![CDATA[<strong>Walls of Books</strong><em>Columbus, GA</em>Monday – Saturday: 10:00 A.M.-7:00 P.M.Sunday: Closed]]></description>
+<source url="https://www.gottwalsbooks.com">https://www.gottwalsbooks.com</source>
+<georss:where>
+<gml:Point>
+<gml:pos>32.507720 -84.953087</gml:pos>
+</gml:Point>
+</georss:where>
+</item>
+</channel>
+</rss>
